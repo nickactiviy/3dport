5743b6b0f09526e5e04aa4ddde3266fe0b69f889
Nick <121074947+nickactiviy@users.noreply.github.com>
2023-09-12 15:57:40 +0700

    main.js

--- main.js
+++ main.js
@@ -317,7 +317,7 @@ function loadIntroText() {
       new THREE.MeshPhongMaterial({ color: 0x171f27, flatShading: true }),
       new THREE.MeshPhongMaterial({ color: 0xffffff }),
     ];
-    const titleGeo = new TextGeometry('SUSHIL THAPA', {
+    const titleGeo = new TextGeometry('NICKDEV', {
       font: font,
       size: 0.08,
       height: 0.01,
@@ -334,7 +334,7 @@ function loadIntroText() {
       new THREE.MeshPhongMaterial({ color: 0xffffff }),
     ];
     const subTitleGeo = new TextGeometry(
-      'Web Designer / Developer / Content Creator',
+      'Web Designer / Developer / Programmer',
       {
         font: font,
         size: 0.018,
